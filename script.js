c228ec861cf890bfea8e52451ec25f225d37e99b
Marek Witkowski <marek.witkowski@onet.com.pl>
2020-08-07 09:41:05 +0200

    add lists done

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const listsContainer = document.querySelector("[data-lists]");
+const newListForm = document.querySelector("[data-new-list-form]");
+const newListInput = document.querySelector("[data-new-list-input]");
 
+const LOCAL_STORAGE_LIST_KEY = "lists";
 let lists = [
   {
     id: 1,
@@ -10,6 +13,20 @@ let lists = [
     name: "Work",
   },
 ];
+
+newListForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const newListName = newListInput.value;
+  if (newListName == null || newListName == "") return;
+  const list = createList(newListName);
+  newListInput.value = null;
+  lists.push(list);
+  render();
+});
+
+function createList(name) {
+  return { id: Date.now().toString(), name: name, tasks: [] };
+}
 
 function render() {
   clearElement(listsContainer);
