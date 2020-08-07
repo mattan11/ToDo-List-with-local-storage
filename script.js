3abe6b55b0b97b04d46b891fba170a7c84bf42aa
Marek Witkowski <marek.witkowski@onet.com.pl>
2020-08-07 09:52:03 +0200

    list local storage done

--- script.js
+++ script.js
@@ -2,17 +2,8 @@ const listsContainer = document.querySelector("[data-lists]");
 const newListForm = document.querySelector("[data-new-list-form]");
 const newListInput = document.querySelector("[data-new-list-input]");
 
-const LOCAL_STORAGE_LIST_KEY = "lists";
-let lists = [
-  {
-    id: 1,
-    name: "Home",
-  },
-  {
-    id: 2,
-    name: "Work",
-  },
-];
+const LOCAL_STORAGE_LIST_KEY = "task.lists";
+let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || [];
 
 newListForm.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -21,11 +12,20 @@ newListForm.addEventListener("submit", (e) => {
   const list = createList(newListName);
   newListInput.value = null;
   lists.push(list);
-  render();
+  saveAndRender();
 });
 
 function createList(name) {
   return { id: Date.now().toString(), name: name, tasks: [] };
+}
+
+function saveAndRender() {
+  save();
+  render();
+}
+
+function save() {
+  localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(lists));
 }
 
 function render() {
